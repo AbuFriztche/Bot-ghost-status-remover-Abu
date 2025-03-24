43f100435c4171eacb8625abed8bc9809b665d1f
AbuFriztche <abufriztche@gmail.com>
2025-03-25 00:22:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Sunucuları İzliyor", "Kontroller Yapıyor"];
+const statusMessages = ["Sunucuları İzliyor", "Heil AS.İZ!"];
 const statusTypes = ['dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
